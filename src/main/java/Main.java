e177c245330824cf6cf8dd629f04baf3273bcfd3
Andrey <89024406064@mail.ru>
2023-10-03 20:58:15 +0500

    Запрос на получение списка фактов о кошках. Исправленное

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,20 +13,24 @@ public class Main {
     public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
     public static final ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+    public static void main(String[] args) {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
                         .setSocketTimeout(30000)
                         .setRedirectsEnabled(false)
                         .build())
                 .build();
-        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-        CloseableHttpResponse response = httpClient.execute(request);
-        List<Cat> cats = mapper.readValue(response.getEntity().getContent(),
-                new TypeReference<>() {
-                });
-        cats.stream().filter(value -> value.getUpvotes() != null && Integer.parseInt(value.getUpvotes()) > 0)
-                .forEach(System.out::println);
+        ) {
+            HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+            CloseableHttpResponse response = httpClient.execute(request);
+            List<Cat> cats = mapper.readValue(response.getEntity().getContent(),
+                    new TypeReference<>() {
+                    });
+            cats.stream().filter(value -> value.getUpvotes() > 0)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
